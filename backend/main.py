970e28a8cbd2d3f8a10a8f5c697617be79a9562d
Artem Dontsov <artem@dontsoft.com>
2022-06-18 21:18:57 +0200

    Added backend

---
+++ backend/main.py
@@ -0,0 +1,120 @@
+from joblib import load
+
+from fastapi import FastAPI
+from sklearn.svm import LinearSVC
+from pydantic import BaseModel
+from typing import Union
+import pandas as pd
+
+app = FastAPI()
+
+svm = load("LocalModel.joblib")
+
+# Calculate text complexity using rounded ARI
+def ARI(text):
+    score = 0.0 
+    len1, len2, len3 = len(text), len(text.split()), len(text.split('.'))
+    if len(text) > 0 and len2 > 0 and len3 > 0:
+        score = round(4.71 * (len1 / len2 ) +  0.5 * ( len2 / len3) - 21.43)
+    return score if score > 0 else 0
+
+
+class RequestItem(BaseModel):
+    is_plus_article: Union[bool, None] = False
+    is_dpa: Union[bool, None] = False
+    pad_pleasure: Union[float, None] = 0.0
+    pad_arousal: Union[float, None] = 0.0
+    pad_dominance: Union[float, None] = 0.0
+    preview_pad_pleasure: Union[float, None] = 0.0
+    preview_pad_arousal: Union[float, None] = 0.0
+    preview_pad_dominance: Union[float, None] = 0.0
+    emo_aerger: Union[float, None] = 0.0
+    emo_erwarten: Union[float, None] = 0.0
+    emo_ekel: Union[float, None] = 0.0
+    emo_furcht: Union[float, None] = 0.0
+    emo_freude: Union[float, None] = 0.0
+    emo_traurigkeit: Union[float, None] = 0.0
+    emo_ueberraschung: Union[float, None] = 0.0
+    emo_vertrauen: Union[float, None] = 0.0
+    preview_emo_aerger: Union[float, None] = 0.0
+    preview_emo_erwarten: Union[float, None] = 0.0
+    preview_emo_ekel: Union[float, None] = 0.0
+    preview_emo_furcht: Union[float, None] = 0.0
+    preview_emo_freude: Union[float, None] = 0.0
+    preview_emo_traurigkeit: Union[float, None] = 0.0
+    preview_emo_ueberraschung: Union[float, None] = 0.0
+    preview_emo_vertrauen: Union[float, None] = 0.0
+    article_header_contains_quote: Union[int, None] = 0.0
+    article_header_contains_question: Union[int, None] = 0.0
+    article_header_contains_doppelpunkt: Union[int, None] = 0.0
+    article_header_contains_pronoun_writer: Union[int, None] = 0.0
+    article_header_contains_pronoun_reader: Union[int, None] = 0.0
+    article_preview_contains_quote: Union[int, None] = 0.0
+    article_preview_contains_question: Union[int, None] = 0.0
+    article_preview_contains_doppelpunkt: Union[int, None] = 0.0
+    article_preview_contains_pronoun_writer: Union[int, None] = 0.0
+    article_preview_contains_pronoun_reader: Union[int, None] = 0.0
+    topic: str
+    locality : str
+    newstype :str
+    genre: str
+    full_text: str
+    portal_id: str
+
+
+# portal_id ['portal_1' 'portal_31' 'portal_17' 'portal_3' 'portal_2']
+# topic: ['Vermischtes: Gesundheit' 'Justiz/Kriminalität' 'Vermischtes: Religion'
+#  'Vermischtes: Soziales' 'Verkehr/Infrastruktur' '' 'Kultur'
+#  'Vermischtes: Sonstiges' 'Vermischtes: Leute' 'Wirtschaft: Unternehmen'
+#  'Vermischtes: Freizeit/Hobbys' 'Sport: Fußball' 'Politik'
+#  'Katastrophe/Unglück' 'Sport: Nicht-Fußball' 'Vermischtes: Wissenschaft'
+#  'Bildung/Erziehung' 'Wirtschaft: Verbraucher']
+# locality: ['National' 'Lokal (Lokalausgabe)' 'Regional (Verbreitungsgebiet)' ''
+#  'International']
+# newstype: ['News to Know' 'News to Entertain' '' 'News to Use']
+# genre: ['Nachrichten/Bericht' 'Meinung' 'Ratgeber/Service' ''
+#  'Reportage/Storytelling' 'Kurzmeldung' 'Porträt' 'Interview'
+#  'Newsblog/Ticker']
+
+PORTAL_VALUES = ['portal_1', 'portal_31', 'portal_17', 'portal_3', 'portal_2']
+PORTAL_PREFIX = 'portal_id_'
+PORTAL_COL = lambda x : f"{PORTAL_PREFIX}{x}"
+TOPIC_VALUES = ['Vermischtes: Gesundheit', 'Justiz/Kriminalität','Vermischtes: Religion', 'Vermischtes: Soziales', 'Verkehr/Infrastruktur', '',  'Kultur', 'Vermischtes: Sonstiges', 'Vermischtes: Leute', 'Wirtschaft: Unternehmen', 'Vermischtes: Freizeit/Hobbys','Sport: Fußball','Politik', 'Katastrophe/Unglück', 'Sport: Nicht-Fußball', 'Vermischtes: Wissenschaft', 'Bildung/Erziehung', 'Wirtschaft: Verbraucher']
+TOPIC_PREFIX = 'topic_'
+TOPIC_COL = lambda x : f"{TOPIC_PREFIX}{x}"
+LOCALITY_VALUES = ['National', 'Lokal (Lokalausgabe)', 'Regional (Verbreitungsgebiet)', '', 'International']
+LOCALITY_PREFIX = 'locality_'
+LOCALITY_COL = lambda x : f"{LOCALITY_PREFIX}{x}"
+NEWS_TYPE_VALUE = ['News to Know', 'News to Entertain', '', 'News to Use']
+NEWS_TYPE_PREFIX = 'newstype_'
+NEWS_TYPE_COL = lambda x : f"{NEWS_TYPE_PREFIX}{x}"
+GENRE_VALUE = ['Nachrichten/Bericht', 'Meinung', 'Ratgeber/Service', '', 'Reportage/Storytelling', 'Kurzmeldung', 'Porträt', 'Interview', 'Newsblog/Ticker']
+GENRE_PREFIX = 'genre_'
+GENRE_COL = lambda x : f"{GENRE_PREFIX}{x}"
+
+@app.post("/")
+async def root(data : RequestItem):
+    
+    _ari = ARI(data.full_text)
+    _dict = data.dict()
+    print(_dict)
+    df = pd.DataFrame(_dict, index=[0])
+    df["ARI"] = _ari
+    for v in TOPIC_VALUES:
+        df[TOPIC_COL(v)] = v == data.topic
+    for v in LOCALITY_VALUES:
+        df[LOCALITY_COL(v)] = v == data.locality
+    for v in NEWS_TYPE_VALUE:
+        df[NEWS_TYPE_COL(v)] = v == data.newstype
+    for v in GENRE_VALUE:
+        df[GENRE_COL(v)] = v == data.genre
+    for v in PORTAL_VALUES:
+        df[PORTAL_COL(v)] = v == data.portal_id
+    df = df.drop(columns=["topic", "locality", "newstype", "genre", "full_text", "portal_id"])
+    print(df.columns)
+
+    prediction = svm.predict(df)
+    print(prediction)
+    return {
+        "value": int(prediction[0])
+    }
---
+++ script.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import pyarrow.parquet as pq
+import matplotlib.pyplot as plt
+import datetime
+import gc
+import glob
+import os
+import seaborn as sns
+from joblib import dump
+
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import train_test_split
+
+
+EMO_COLUMNS = ["pad_pleasure", "pad_arousal", "pad_dominance", "preview_pad_pleasure", "preview_pad_arousal", "preview_pad_dominance", "emo_aerger", "emo_erwarten", "emo_ekel", "emo_furcht", "emo_freude", "emo_traurigkeit", "emo_ueberraschung", "emo_vertrauen", "preview_emo_aerger", "preview_emo_erwarten", "preview_emo_ekel", "preview_emo_furcht", "preview_emo_freude", "preview_emo_traurigkeit", "preview_emo_ueberraschung", "preview_emo_vertrauen"]
+PLAIN_TEXT_COLUMNS = ["article_header", "article_teaser", "article_full_text", "article_preview_emotion"]
+COLUMNS_TO_DROP = ["published_at_local", "modified_at_local", "publisher_id"]
+ARTICLE_DATA_PATH = lambda x: f"C:/hackathon/Techlabs Hackathon/Drive Daten/students_articles_enriched_{x}.parquet.gzip"
+PAGEVIEW_DATA_PATH = "C:/hackathon/Techlabs Hackathon/Drive Daten/students_pageviews_2021-01-02.parquet.gzip"
+gc.collect()
+
+def get_first_batch(path):
+    parquet_file = pq.ParquetFile(path)
+    for batch in parquet_file.iter_batches():
+        return batch.to_pandas()
+def get_all_batches(path):
+    parquet_file = pq.ParquetFile(path)
+    return pd.concat([batch.to_pandas() for batch in parquet_file.iter_batches()])
+
+def patch_article_full_text(df):
+    df["ARI"] = df["article_full_text"].apply(lambda x: x.replace("\n", " ")).apply(lambda x:ARI(x))
+    df = df.drop(columns=PLAIN_TEXT_COLUMNS)
+    df[["topic", "locality", "newstype", "genre"]] = df[["topic", "locality", "newstype", "genre"]].fillna("")
+    df[EMO_COLUMNS] = df[EMO_COLUMNS].fillna(0)
+    df[["is_plus_article"]] = df[["is_plus_article"]].fillna(False)
+    return df
+
+# Calculate text complexity using rounded ARI
+def ARI(text):
+    score = 0.0 
+    len1, len2, len3 = len(text), len(text.split()), len(text.split('.'))
+    if len(text) > 0 and len2 > 0 and len3 > 0:
+        score = round(4.71 * (len1 / len2 ) +  0.5 * ( len2 / len3) - 21.43)
+    return score if score > 0 else 0
+
+article_data = patch_article_full_text(pd.concat([get_all_batches(ARTICLE_DATA_PATH(i)) for i in range(1, 5)]))
+pageview_data = pd.concat([get_first_batch(f) for f in glob.glob("C:/hackathon/Techlabs Hackathon/Drive Daten/students_pageviews*.gzip")])
+pageview_data = get_first_batch(PAGEVIEW_DATA_PATH)
+
+pageview_data_grouped = pageview_data[["article_drive_id", "time_engaged_in_s"]].groupby(["article_drive_id"]).sum(["time_engaged_in_s"]).reset_index()
+
+merged_data = pd.merge(article_data, pageview_data_grouped, on="article_drive_id")
+merged_data["is_plus_article"].fillna(False, inplace=True)
+merged_data = pd.merge(merged_data, pageview_data[["article_drive_id", "portal_id"]], on="article_drive_id")
+
+merged_data_cleaned_no_dummies = merged_data.drop(columns=COLUMNS_TO_DROP)
+features = list(filter(lambda x: x not in ["time_engaged_in_s", "article_drive_id"], merged_data_cleaned_no_dummies.columns))
+merged_data_cleaned_no_dummies_no_na = merged_data_cleaned_no_dummies.dropna()
+X, y = pd.get_dummies(merged_data_cleaned_no_dummies_no_na[features]), merged_data_cleaned_no_dummies_no_na["time_engaged_in_s"].array
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+svm = LinearSVC()
+svm.fit(X_train, y_train)
+print(svm.score(X_test, y_test))
+y_pred = svm.predict(X_test)
+sns.histplot(y_pred)
+sns.histplot(y_test)
+
+dump(svm, "LinearSVC.joblib")
